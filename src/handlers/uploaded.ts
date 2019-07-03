32e1570f8730f4d7ee300c4f58b63893710bebf0
Oleksandr Stepanenko <ostepanenko@intellias.com>
2019-07-03 20:30:22 +0300

    uploaded endpoint added

---
+++ src/handlers/uploaded.ts
@@ -0,0 +1,20 @@
+import { waterfall, AsyncResultCallback } from "async";
+import { readdir } from "fs";
+import { Request, Response } from "express";
+import { resolve } from "path";
+
+export default (req: Request, res: Response) => {
+  const uploadDirPath = resolve(process.env.PWD, "uploads");
+  readdir(uploadDirPath, (err: Error, list) => {
+    if (err) res.status(500).send("Something went wrong");
+    if (list.length > 0)
+      res
+        .type("application/json")
+        .status(200)
+        .send(JSON.stringify({ fileNames: list.sort() }));
+    else {
+      res.statusMessage = "No data";
+      res.status(204).send();
+    }
+  });
+};
--- src/router.ts
+++ src/router.ts
@@ -2,15 +2,63 @@ import { Router, Request, Response } from "express";
 import { getLogger } from "./utils/logger";
 import { resolve } from "path";
 import { getDateTime, getUser } from "./utils";
-import qs from "querystring";
-import { IncomingForm, Fields, Files } from "formidable";
+import { IncomingForm } from "formidable";
 import { getExtension } from "mime";
 import { Client } from "pg";
 import { createHash } from "crypto";
 import { createReadStream } from "fs";
+import uploadHandler from "./handlers/uploaded";
 
 const router = Router();
 const logger = getLogger(resolve(__dirname, "..", "logs"));
+
+router.post("/api/user/:id", (req: Request, res: Response) => {
+  console.log(
+    req.params,
+    // req.query,
+    // req.route,
+    req.body,
+    // req.cookies,
+    // req.signedCookies,
+    // req.ip,
+    // req.xhr,
+    // req.acceptsLanguages()
+    req.query
+    // req.headers
+  );
+  res.redirect(303, "/");
+  // res.redirect(303, "/user"); // .get() need
+});
+
+router.get("/api/uploaded", (req: Request, res: Response) => {
+  uploadHandler(req, res);
+});
+
+router.post("/api/upload", (req: Request, res: Response) => {
+  const form = new IncomingForm();
+  form.parse(req);
+  form.on("fileBegin", (name, file) => {
+    if (file.type === "image/jpeg") {
+      file.path = resolve(
+        __dirname,
+        "..",
+        "uploads",
+        `${name}.${getExtension(file.type)}`
+      );
+    } else {
+      req.socket.end();
+      res.status(400).send();
+    }
+  });
+  form.on("file", (name, file) => {
+    console.log(`Uploaded: ${name}.${getExtension(file.type)}, ${file.type}`);
+    res.end();
+  });
+});
+
+// router.get("/api/path/is/here", (req: Request, res: Response) => {
+//   res.status(200).send("Everysing is fine!");
+// });
 
 router.get("/api_old", async (req: Request, res: Response) => {
   const client = new Client();
@@ -42,49 +90,5 @@ router.get("/api_old", async (req: Request, res: Response) => {
     res.send();
   });
 });
-
-router.post("/api/user/:id", (req: Request, res: Response) => {
-  console.log(
-    req.params,
-    // req.query,
-    // req.route,
-    req.body,
-    // req.cookies,
-    // req.signedCookies,
-    // req.ip,
-    // req.xhr,
-    // req.acceptsLanguages()
-    req.query
-    // req.headers
-  );
-  res.redirect(303, "/");
-  // res.redirect(303, "/user"); // .get() need
-});
-
-router.post("/api/upload", (req: Request, res: Response) => {
-  const form = new IncomingForm();
-  form.parse(req);
-  form.on("fileBegin", (name, file) => {
-    if (file.type === "image/jpeg") {
-      file.path = resolve(
-        __dirname,
-        "..",
-        "uploads",
-        `${name}.${getExtension(file.type)}`
-      );
-    } else {
-      req.socket.end();
-      res.status(400).send();
-    }
-  });
-  form.on("file", (name, file) => {
-    console.log(`Uploaded: ${name}.${getExtension(file.type)}, ${file.type}`);
-    res.end();
-  });
-});
-
-// router.get("/api/path/is/here", (req: Request, res: Response) => {
-//   res.status(200).send("Everysing is fine!");
-// });
 
 export default router;
